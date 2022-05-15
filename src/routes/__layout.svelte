742d18b6071c8d6705add3dba0629ebb4ec05ab1
apttx <git@aptt.ch>
2022-05-15 22:46:52 +0200

    feat: 🎸 add root layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<slot />
+
+<style global lang="postcss">
+  @import '@fontsource/manrope/400.css';
+  @import '@fontsource/manrope/700.css';
+  @import '@fontsource/azeret-mono/400.css';
+  @import '@fontsource/azeret-mono/700.css';
+
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
