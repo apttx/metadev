6f6d4dd14fde78fb90c03a9de749705324992cf5
apttx <git@aptt.ch>
2022-05-15 22:46:37 +0200

    feat(components): 🎸 add component with colorable label (warn, danger)
    displays a label above its content & provides boolean props for warn &
    danger states that add icons, change colour & reverse the fill

---
+++ src/lib/components/LabeledTile.svelte
@@ -0,0 +1,33 @@
+<script>
+  import ExclamationCircle from './heroicons/ExclamationCircle.svelte'
+  import ExclamationTriangle from './heroicons/ExclamationTriangle.svelte'
+
+  export let label
+  export let danger = false
+  export let warn = false
+</script>
+
+<div class="relative">
+  <span
+    class="inline-flex items-center transition border-primary border-2 rounded-md text-primary bg-primer px-1 font-bold text-sm mb-2 space-x-1"
+    class:text-primer={warn || danger}
+    class:border-warn={warn}
+    class:bg-warn={warn}
+    class:border-danger={danger}
+    class:bg-danger={danger}
+  >
+    <span class="inline-block">
+      {label}
+    </span>
+
+    {#if warn}
+      <ExclamationTriangle />
+    {/if}
+
+    {#if danger}
+      <ExclamationCircle />
+    {/if}
+  </span>
+
+  <slot />
+</div>
