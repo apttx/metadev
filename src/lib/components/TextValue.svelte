1a13a6938456255d37633d8b884befa85f9d96f8
apttx <git@aptt.ch>
2022-05-15 22:44:55 +0200

    feat(components): 🎸 add text value component with placeholder fnality
    displays a loading animation while the value is undefined & provides
    slots for when the value is defined or null

---
+++ src/lib/components/TextValue.svelte
@@ -0,0 +1,26 @@
+<script>
+  let className = ''
+  export { className as class }
+  export let value = undefined
+
+  $: isUndefined = value === undefined
+</script>
+
+<p
+  class={className}
+  class:block={!value}
+  class:italic={!value}
+  class:text-confident={!value}
+  class:font-normal={!value}
+  class:bg-confident={isUndefined}
+  class:animate-pulse={isUndefined}
+  class:pr-1={isUndefined}
+>
+  {#if value}
+    <slot>
+      {value}
+    </slot>
+  {:else}
+    <slot name="no-value">No value</slot>
+  {/if}
+</p>
