7743bc13fc4ee2f5c6c7056b3ff73a4e2360bd82
apttx <git@aptt.ch>
2022-05-15 22:43:23 +0200

    feat(components): 🎸 add language component displaying language & code
    uses the Intl to get the language name from a code & displays the code
    as a tag/label after it

---
+++ src/lib/components/Language.svelte
@@ -0,0 +1,14 @@
+<script context="module">
+  const languageDisplayName = new Intl.DisplayNames(['en'], {
+    type: 'language',
+  })
+</script>
+
+<script>
+  export let language
+</script>
+
+{languageDisplayName.of(language)}
+<span class="font-mono bg-confident/10 px-2 py-1 rounded-md text-sm">
+  {language}
+</span>
